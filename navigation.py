3cf1a79dc513b47e2891e50d96ddc6afef63a213
Max <laplandersan@ya.ru>
2025-05-28 22:35:27 +0300

    Fix plugin use when System proxy enabled
    When `network.usehttpproxy` settings is set, python environment makes all urllib.request.urlopen() calls through that `network.httpproxyserver`. Navigation part of plugin couldn't access service API through that proxy. 
    So to make API call to go directly 'no_proxy' environment should be provided.

--- navigation.py
+++ navigation.py
@@ -3,4 +3,7 @@ import sys
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'resources', 'site-packages'))
 from elementum import navigation
 
+# Make sure urlopen to ELEMENTUMD service will always go directly
+os.environ[no_proxy] = 'localhost,127.0.0.1'
+
 navigation.run()
